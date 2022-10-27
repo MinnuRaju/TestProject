7237508f256a912f87e1278bce3e094112754a0b
MINNU <MINNU@minnu>
2022-10-28 00:02:31 +0400

    Second commit

--- com.supermarket/src/main/java/com/pom/supermarket/pages/ManageLocation.java
+++ com.supermarket/src/main/java/com/pom/supermarket/pages/ManageLocation.java
@@ -1,5 +1,7 @@
 package com.pom.supermarket.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,7 +18,7 @@ public class ManageLocation {
 WebElement manageloc;
 @FindBy(xpath="//a[@class='btn btn-rounded btn-danger']")
 WebElement newbtn;
-@FindBy(xpath="//select[@name='country_id']")
+@FindBy(xpath="//select[@name='country_id']")     
 public
 WebElement countrydrop;
 @FindBy(xpath="//select[@name='state_id']")
@@ -47,21 +49,39 @@ WebElement searchbtn;
 WebElement editalert;
 @FindBy(xpath="//button[@name='update']")
 WebElement updatebtn;
+@FindBy(xpath="//a[@class='btn btn-rounded btn-primary']")
+public
+WebElement search;
+@FindBy(xpath="//input[@name='location']")
+WebElement locationinput;
+@FindBy(xpath="//table[@class='table table-bordered table-hover table-sm']/tbody/tr")
+List<WebElement> rElement;
+@FindBy(xpath="//table[@class='table table-bordered table-hover table-sm']/tbody/tr[1]/td")
+List<WebElement> cElement;
+String tPath="//table[@class='table table-bordered table-hover table-sm']/tbody";
+@FindBy(xpath="//button[@name='Search']")
+WebElement searchbtnclick;
+@FindBy(xpath="//a[@class='btn btn-sm btn btn-danger btncss'][1]")
+WebElement delete;
+@FindBy(xpath="//div[@class='alert alert-success alert-dismissible']")
+WebElement deletealert;
+@FindBy(xpath="//a[@class='btn btn-default btn-fix']")
+public
+WebElement resetsearch;
+
 
     public void createNew() {
 	       manageloc.click();
 	       newbtn.click();
 	
-}
+          }
      public String locationAndDeliverdata(String loc, String deliverycharge) {
              location.sendKeys(loc);
  	         delivery.sendKeys(deliverycharge);
              savebtn.click();
  	         return alerttext.getText();
- 	     
-
-      
-}
+ 
+          }
      public String edit(String locationedit,String editdelivery ) {
     	 location.clear();
     	 location.sendKeys(locationedit);
@@ -70,7 +90,6 @@ WebElement updatebtn;
     	 updatebtn.click();
     	 return editalert.getText();
     	 
-    
      }
  	public ManageLocation(WebDriver driver) {
 		this.driver=driver;
@@ -78,4 +97,21 @@ WebElement updatebtn;
 		
 		
 	}
+ 	public void searchItem(String locationin) {
+ 		locationinput.sendKeys(locationin);
+ 		searchbtnclick.click();
+ 	
+ 	}
+ 	public String deleteItem() {
+ 		delete.click();
+ 		general=new GeneralUtility();
+ 		general.alertAccept(driver);
+ 		return deletealert.getText();
+ 		
+ 	}
+ 	public String tableTest(int rowstart,int columnstart,String expected) {
+		general=new GeneralUtility();
+		return general.tableIteration(driver, rElement, cElement, rowstart, columnstart, expected, tPath);
+		
+}
 }
